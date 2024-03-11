bd8c0213d219b05c72f1418795238a5e41f070c9
Ingi <ingiharaldss@gmail.com>
2024-03-11 20:11:16 +0000

    fótboltaleikur

--- .idea/lib/gameclasses.dart
+++ .idea/lib/gameclasses.dart
@@ -1,35 +1,115 @@
+import 'dart:math';
+
 void sayHello() {
-print("hello");
-
+  print("Hello");
 }
-
 
 class footballPlayer {
-int id;
-String firstName;
-String lastName;
-String playerpos;
-int squadNumber;
+  int id;
+  String firstName;
+  String lastName;
+  position playerpos;
+  int squadNumber;
 
-
-footballPlayer({required this.id, required this.firstName, required this.lastName, required this.playerpos, required this.squadNumber});
-
+  footballPlayer({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.playerpos,
+    required this.squadNumber
+  });
 }
 
-class postition{
-int id;
-String name;
-postition({required this.id, required this.name});
+class position {
+  int id;
+  String name;
+  int scoreOdds;
+
+  position({
+    required this.id,
+    required this.name,
+    required this.scoreOdds
+  });
 
 }
 
 class footballTeam {
-int id;
-String name;
-String league;
-String country;
-List<footballPLayers> players;
+  int id;
+  String name;
+  String league;
+  String country;
+  List<footballPlayer> players;
 
-footballTeam({required this.id, required this.name, required this.league, required this.country, required this.players});
+  footballTeam({
+    required this.id,
+    required this.name,
+    required this.league,
+    required this.country,
+    required this.players
+  });
 
+  void showTeam() {
+    print("The ${this.name} Lineup");
+    this.players.forEach((element){
+      print("${element.squadNumber} - ${element.firstName} ${element.lastName}");
+    });
+  }
+}
+
+
+
+class footballGame {
+  footballTeam homeTeam;
+  footballTeam awayTeam;
+  int homeScore;
+  int awayScore;
+  int time;
+
+  footballGame({
+    required this.homeTeam,
+    required this.awayTeam,
+    this.homeScore = 0,
+    this.awayScore = 0,
+    this.time = 0
+  });
+
+  void showLineUp(){
+    homeTeam.showTeam();
+    awayTeam.showTeam();
+  }
+
+  void showScore() {
+    print("${this.time}: ${this.homeTeam.name} - ${this.homeScore}:${this.awayScore} -  ${this.awayTeam.name}");
+  }
+
+  void playGame(int minute) {
+    this.time = minute;
+    showScore();
+    var random = Random();
+    int randomNumb1 = random.nextInt(11);
+    int randomNumb2 = random.nextInt(11);
+    if (randomNumb1 == randomNumb2) {
+      if (randomNumb1 > 6) {
+        bool didHeScore = whoScored(homeTeam.players, randomNumb1);
+        if (didHeScore) {
+          this.homeScore++;
+        }
+      } else {
+        bool didHeScore = whoScored(awayTeam.players, randomNumb1);
+        if (didHeScore) {
+          this.awayScore++;
+        }
+      }
+    }
+  }
+  bool whoScored(List<footballPlayer> tempPlayers, int numb) {
+    bool results = false;
+    var random = Random();
+    int randomNumb1 = random.nextInt(tempPlayers[numb].playerpos.scoreOdds);
+    int randomNumb2 = random.nextInt(tempPlayers[numb].playerpos.scoreOdds);
+    if(randomNumb1 == randomNumb2) {
+      print("Goal for player ${tempPlayers[numb].lastName}");
+    }
+    return results = true;
+  }
 }
