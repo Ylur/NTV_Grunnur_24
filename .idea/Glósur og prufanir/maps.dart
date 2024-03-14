f1f9613a2ffe71ff52cd3c2cbe0935c72f744faf
Ingi <ingiharaldss@gmail.com>
2024-03-14 22:14:51 +0000

    glósur

--- .idea/Glósur og prufanir/maps.dart
+++ .idea/Glósur og prufanir/maps.dart
@@ -1,9 +1,30 @@
 import 'dart:io';
 
 void main() {
-// key og value pörun
+// key og value pörun td Ingi er key og piparostur er value
 var toppings = {"Ingi":"Piparostur", "Nína":"Pepperoni"};
+// prenta lista
 print("${toppings}");
+//sýna mitt value
 print("${toppings["Ingi"]}");
+// sýna values
+print("${toppings.values}");
+//sýna keys
+print("${toppings.keys}");
+//Sýna fjölda
+print("${toppings.length}");
+//bæta við
+toppings ["Dísa"] = "banana";
+print("${toppings}");
+// bæta mörgu við
+toppings.addAll({"Pabbi":"Rjómaostur", "Mamma":"Ananas"});
+print("${toppings}");
+//fjarlægja úr listanum
+toppings.remove("Ingi");
+print("${toppings}");
+//tæma listann alveg
+toppings.clear();
+print("${toppings}");
+
 
 }
